7a36fd6c58c03e44e736494819c2c0c9600b9471
theussoliveira <matheusfotografia987@gmail.com>
2022-06-21 15:50:23 -0300

    :package: menu side bar

---
+++ javascript/script.js
@@ -0,0 +1,5 @@
+document.querySelector(".hamburguer").addEventListener("click", () => document.querySelector(".container").classList.toggle('show-menu'));   
+
+
+/*Selecionando a classe ".hamburguer", adicionando um evento a essa classe, o "click", logo após o clique executa a função de selecionar a classe ".container"
+*e adicionar uma classe dentro do ".container" que é a "show-menu" dando interatividade ao meu menu.*/
